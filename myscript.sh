55b94af720ebaefa4bc4d7db9d31c94a32ad75f2
Halla Raafat <halla.raafat2000@gmail.com>
2023-12-04 16:09:05 +0200

    Task1

---
+++ myscript.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cd ~
+mkdir -p  myDirectory/secoundDirectory
+cd ~/myDirectory/secoundDirectory 
+touch myNotePaper
+cp myNotePaper ~/myDirectory 
+mv ~/myDirectory/myNotePaper ~/myDirectory/myOldNote
+ 
+
